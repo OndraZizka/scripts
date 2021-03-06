5559a9a9f746b0a5fe69c0dd86da36e263837e4f
Ondrej Zizka <ondrej_zizka@epam.com>
2021-03-06 19:17:52 +0100

    Improve checkDuplicates

--- general/checkDuplicates.sh
+++ general/checkDuplicates.sh
@@ -1,11 +1,31 @@
-#for i in `find -type f -size +1M`; do
-find -type f -size +3M -print0 | while IFS= read -r -d '' i; do
-  #echo $i
-  echo -n '.'
-  if grep -q "$i" md5-partial.txt; then echo -e "\n$i  ---- Already counted, skipping."; continue; fi
-  #md5sum "$i" >> md5.txt
-  MD5=`dd bs=1M count=1 if="$i" status=noxfer | md5sum`
-  MD5=`echo $MD5 | cut -d' ' -f1`
-  if grep "$MD5" md5-partial.txt; then echo "\n$i  ----   Duplicate!"; fi
-  echo $MD5 $i >> md5-partial.txt
+
+VERBOSE=false
+
+find -type f -size +3M -print0 | while IFS= read -r -d '' PATHX; do
+    if [ true != "$VERBOSE" ] ; then echo -n '.'; fi
+    
+    if grep -q "$PATHX" md5-partial.txt; then 
+        ##  Checksum already computed.
+        #echo -e "\n$PATHX checksum already computed.";
+        MD5=`grep "$PATHX" md5-partial.txt | cut -f1`
+        
+        OCCURENCES=`grep "$MD5" md5-partial.txt`
+        COUNT=`echo "$OCCURENCES" | wc -l`
+        if [ "$COUNT" != 1 ] ; then 
+            echo -e "\n $COUNT duplicates:\n$OCCURENCES"; 
+        fi
+    else
+        SIZE=`du -h "$PATHX" | cut -f1`
+        MD5=`dd bs=1M count=1 if="$PATHX" status=noxfer | md5sum`
+        MD5=`echo $MD5 | cut -d' ' -f1`    
+        echo "$MD5 $PATHX" >> md5-partial.txt
+
+        OCCURENCES=`grep "$MD5" md5-partial.txt`
+        COUNT=`echo "$OCCURENCES" | wc -l`
+        if [ "$COUNT" != 1 ] ; then 
+            echo -e "\n $COUNT duplicates: Size: `du -h "$PATHX" | cut -f1`\n$OCCURENCES"; 
+        fi
+    fi
 done
+
+echo -e "\n"
